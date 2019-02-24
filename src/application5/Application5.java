6c1ab8ce8d92d6656d430d914bfe1d6b62ad036b
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-02-24 23:09:21 +0530

    application5
    application5

---
+++ src/application5/Application5.java
@@ -0,0 +1,18 @@
+
+package application5;
+
+
+public class Application5 {
+
+   
+    public static void main(String[] args) {
+      int subject1= 20;
+      int subject2= 25;
+      if((subject1>=35)||(subject2>=35)){
+          System.out.println("the condition is true");
+      }else{
+          System.out.println("the condition is false");
+      }
+    }
+    
+}
